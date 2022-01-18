92e30fdb01ce78333db0e46146dbf9e3a1cb06ea
Alireza Hooshyar <hooshyar.net@gmail.com>
2022-01-18 18:48:32 +0330

    Updates

---
+++ precache-manifest.1fb334a0a99eb0daade95e8be3647209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841271fa74a96b42e8236caa3f941196",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03aad622a53c06c6dbc9",
+    "url": "./static/css/main.3d1906d3.chunk.css"
+  },
+  {
+    "revision": "ce6d797eeb3ee20bdda3",
+    "url": "./static/js/2.b88dd413.chunk.js"
+  },
+  {
+    "revision": "bdad44f45bc26a66a15dafbc3d3cfe58",
+    "url": "./static/js/2.b88dd413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03aad622a53c06c6dbc9",
+    "url": "./static/js/main.f9c3e81d.chunk.js"
+  },
+  {
+    "revision": "e517f32fa7a694bce1f2",
+    "url": "./static/js/runtime-main.68ee3d78.js"
+  }
+]);
